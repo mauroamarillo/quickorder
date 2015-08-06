d5987a4bb156c5e207d80f546e99616eae8379fb
Jean <Jean@Jean-Pc>
2015-08-06 18:52:37 -0300

    Merge origin/master
    Conflicts:
    	QuickOrder/src/QuickOrder.java

--- QuickOrder/src/QuickOrder.java
+++ QuickOrder/src/QuickOrder.java
@@ -16,7 +16,8 @@ public class QuickOrder {
     public static void main(String[] args) {
         // TODO code application logic here
         // Jean puto
-        //culito
+        // culito
+        // comite el puch
     }
     
 }
